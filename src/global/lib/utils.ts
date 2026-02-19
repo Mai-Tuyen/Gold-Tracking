80b663e3b3b2b4d555c0ffdaf27bc3d76bb27d04
Mai Tuyen <maituyen4496@gmail.com>
2026-02-19 10:07:27 +0700

    init base

--- src/global/lib/utils.ts
+++ src/global/lib/utils.ts
@@ -1,6 +1,36 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function generateSEOMetadata({
+  title,
+  description,
+  url,
+  thumbnailUrl
+}: {
+  title: string
+  description: string
+  url: string
+  thumbnailUrl: string
+}) {
+  return {
+    title: title,
+    description: description,
+    openGraph: {
+      type: 'website',
+      url: url,
+      title: title,
+      description: description,
+      images: [{ url: thumbnailUrl, width: 1200, height: 630 }]
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: title,
+      description: description,
+      images: [{ url: thumbnailUrl }]
+    }
+  }
 }
